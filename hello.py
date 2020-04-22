1a59c1d742373f1d948e338dd5580178076ba8f0
wop5008 <wonnist1987@gmail.com>
2020-04-22 15:23:48 +0900

    feat:everything change

--- hello.py
+++ hello.py
@@ -1,7 +1,11 @@
 for i in range (1, 100):
+    if i%15== 0:
+        print("fizz buzz)
     if i%3 == 0: 
        print("fizz")
     else:
-       print(i)
+        print(i)
+    elif i%5 == 0:
+        print("buzz")
     
 
