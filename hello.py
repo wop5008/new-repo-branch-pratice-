c03d365cdc2af15b717d3276493c690f8348be6f
wop5008 <wonnist1987@gmail.com>
2020-04-22 15:39:55 +0900

    feat: this has 4 stages

--- hello.py
+++ hello.py
@@ -1,11 +1,10 @@
 for i in range (1, 100):
     if i%15== 0:
-        print("fizz buzz)
-    if i%3 == 0: 
+        print("fizz buzz")
+    elif i%3 == 0: 
        print("fizz")
-    else:
-        print(i)
     elif i%5 == 0:
         print("buzz")
-    
+    else:
+        print(i)
 
